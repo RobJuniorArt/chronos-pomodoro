3929cc829513635fb1bbce9163712d4ee6b644c0
Rob Junior <robson.fsj@gmail.com>
2026-08-18 23:23:24 -0300

    update: adding css styles on heading page and fix type issues on props

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { TimerIcon } from "lucide-react";
 import { Heading } from "./components/Heading";
 
 import "./styles/theme.css";
@@ -6,8 +7,18 @@ import "./styles/global.css";
 function App() {
   return (
     <div>
-      <Heading>Olá Mundo 1</Heading>
-      <Heading>Olá Mundo 2</Heading>
+      <Heading>
+        Olá Mundo 1
+        <button>
+          <TimerIcon />
+        </button>
+      </Heading>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsa iusto
+        aspernatur dignissimos explicabo deserunt quod quam, ipsam, repellat
+        placeat illum nobis nostrum dolor, nihil dolore veritatis neque corrupti
+        tempora assumenda!
+      </p>
     </div>
   );
 }
