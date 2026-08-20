f72ff17d4256bbbf2e17dd02081152e3d5902d6a
Rob Junior <robson.fsj@gmail.com>
2026-08-19 23:10:56 -0300

    Update: create structure for sessions of app

--- src/main.tsx
+++ src/main.tsx
@@ -5,11 +5,5 @@ import { App } from "./App";
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
-    <p>
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Mollitia, vel,
-      tenetur dignissimos, perspiciatis amet esse magni earum deserunt
-      cupiditate nam temporibus error vitae. Earum consequuntur veniam a debitis
-      fugiat suscipit!
-    </p>
   </StrictMode>,
 );
