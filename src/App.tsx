5a44d1a00eb8a1c94570dcb093985945f195d2bd
Rob Junior <robson.fsj@gmail.com>
2026-08-22 13:36:33 -0300

    Update: creation of form for tasks and start css customization

--- src/App.tsx
+++ src/App.tsx
@@ -17,6 +17,24 @@ function App() {
       <Container>
         <CountDown />
       </Container>
+      <Container>
+        <form className="form" action="">
+          <div className="formRow">
+            <label htmlFor="">task</label>
+            <input id="meuInput" type="text" />
+          </div>
+          <div className="formRow">
+            <p>Lorem, ipsum dolor sit amet</p>
+          </div>
+          <div className="formRow">
+            <p>Ciclos</p>
+            <p>0 0 0 0 0 0 0 0 </p>
+          </div>
+          <div className="formRow">
+            <button>Enviar</button>
+          </div>
+        </form>
+      </Container>
     </div>
   );
 }
