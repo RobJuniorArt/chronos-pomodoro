4f9b2b84d30871d5aa50fa7fafd2922d724fe4d8
Rob Junior <robson.fsj@gmail.com>
2026-09-02 09:56:15 -0300

    create system of icon tasks cycle

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -66,9 +66,11 @@ export function MainForm() {
       <div className="formRow">
         <p>próximo intervalo 25min</p>
       </div>
-      <div className="formRow">
-        <Cycles />
-      </div>
+      {state.currentCycle > 0 && (
+        <div className="formRow">
+          <Cycles />
+        </div>
+      )}
       <div className="formRow">
         <DefaultButton icon={<PlayCircleIcon />} />
       </div>
